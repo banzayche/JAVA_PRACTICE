4fc6ce75a623b13e2d9c1287be76cf237ba033c5
Serhii <dharm.serg@gmail.com>
2017-02-14 00:22:13 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Created by admin on 2/13/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\n Try Collections:");
+        RecursiveTest newRecursivetest = new RecursiveTest();
+
+        System.out.println(newRecursivetest.recursiveRun(4));
+    }
+}
