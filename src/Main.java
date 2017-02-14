700f9b9c521bd5847c82e123d74ca8b0fc5397ce
Serhii Prokopchuk <dharm.serg@gmail.com>
2017-02-14 13:13:29 +0200

    Methods refactoring.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by admin on 2/13/17.
  */
 public class Main {
     public static void main(String[] args) {
         System.out.println("\n Try Collections:");
-        RecursiveTest newRecursivetest = new RecursiveTest();
-
-        System.out.println(newRecursivetest.recursiveRun(4));
+        Map<String, String> newIntegers = new HashMap<>();
+        TryKaracubaMethod newTryKaracubaMethod = new TryKaracubaMethod();
+        newIntegers.put("a", "1");
+        newIntegers.put("b", "2");
+        newIntegers.put("c", "3");
+        newIntegers.put("d", "4");
+//
+        System.out.println(newTryKaracubaMethod.calculateThisShit(newIntegers));
+//        System.out.println(newTryKaracubaMethod.devide(15223));
     }
 }
--- src/TryKaracubaMethod.java
+++ src/TryKaracubaMethod.java
@@ -39,26 +39,26 @@ public class TryKaracubaMethod {
     }
 
     // Simple solution for algorithm
-    public int calculateThisShit(Map<String, Integer> numbers){
+    public int calculateThisShit(Map<String, String> numbers){
         // Define the length of integer
-        Integer n = (String.valueOf(numbers.get("a"))+String.valueOf(numbers.get("b"))).length();
+        Integer n = (numbers.get("a")+numbers.get("b")).length();
 
         // Get sum to define length of Integer
-        Integer abSum = numbers.get("a") + numbers.get("b");
-        Integer cdSum = numbers.get("c") + numbers.get("d");
+        Integer abSum = Integer.valueOf(numbers.get("a")) + Integer.valueOf(numbers.get("b"));
+        Integer cdSum = Integer.valueOf(numbers.get("c")) + Integer.valueOf(numbers.get("d"));
 
+        Integer a_plus_b_multiply_c_plus_d = 0;
         // Call recursion or not
-        Integer a_plus_b_multiply_c_plus_d;
         if(String.valueOf(abSum).length() > 2 || String.valueOf(cdSum).length() > 2) {
             // TODO call recursion in right way !!!
-            // a_plus_b_multiply_c_plus_d = calculateThisShit();
+//             a_plus_b_multiply_c_plus_d = calculateThisShit();
         } else{
             a_plus_b_multiply_c_plus_d = abSum * cdSum;
         }
 
         // Simple multiply
-        Integer ac = numbers.get("a") * numbers.get("c");
-        Integer bd = numbers.get("b") * numbers.get("d");
+        Integer ac = Integer.valueOf(numbers.get("a")) * Integer.valueOf(numbers.get("c"));
+        Integer bd = Integer.valueOf(numbers.get("b")) * Integer.valueOf(numbers.get("d"));
 
         // Karacuba method
         Double result = Math.pow(10, n) * ac + Math.pow(10, n/2) * Math.abs(a_plus_b_multiply_c_plus_d - ac - bd) + bd;
@@ -67,34 +67,37 @@ public class TryKaracubaMethod {
     }
 
 
-    public Map<String, Integer> devide(int newInteger){
-        Map<String, Integer> newIntegers = new HashMap<>();
+    public Map<String, String> devide(int newInteger){
+        Map<String, String> newIntegers = new HashMap<>();
         String value = String.valueOf(newInteger);
+
+        if((value.length() % 4) != 0) {
+            while (value.length() % 4 != 0) {
+                value = "0" + value;
+            }
+        }
         int length = value.length();
 
-        if((length % 2) == 0) {
+        String generalFirstPart = value.substring(0, length/2);
+        System.out.println("generalFirstPart = " + generalFirstPart);
 
-            String firstPart = value.substring(0, length/2);
-            String secondPart = value.substring(length/2, length);
+        String firstPart = generalFirstPart.substring(0, generalFirstPart.length()/2);
+        String secondPart = generalFirstPart.substring(generalFirstPart.length()/2, generalFirstPart.length());
+        System.out.println("firstPart = " + firstPart);
+        System.out.println("secondPart = " + secondPart);
 
-            newIntegers.put("a", Integer.valueOf(firstPart));
-            newIntegers.put("b", Integer.valueOf(secondPart));
+        String generalSecondPart = value.substring(length/2, length);
+        System.out.println("generalSecondPart = " + generalSecondPart);
 
-        } else if(length == 1) {
+        String thirdPart = generalSecondPart.substring(0, generalSecondPart.length()/2);
+        String fourthPart = generalSecondPart.substring(generalSecondPart.length()/2, generalSecondPart.length());
+        System.out.println("thirdPart = " + thirdPart);
+        System.out.println("fourthPart = " + fourthPart);
 
-            newIntegers.put("a", Integer.valueOf(value));
-            newIntegers.put("b", Integer.valueOf("0"));
-
-        } else {
-            int rightLength = length - (length % 2);
-
-            String firstPart = value.substring(0, rightLength/2);
-            String secondPart = value.substring(rightLength/2, length);
-
-            newIntegers.put("a", Integer.valueOf(firstPart));
-            newIntegers.put("b", Integer.valueOf(secondPart));
-
-        }
+        newIntegers.put("a", firstPart);
+        newIntegers.put("b", secondPart);
+        newIntegers.put("c", thirdPart);
+        newIntegers.put("d", fourthPart);
 
         return newIntegers;
     }
