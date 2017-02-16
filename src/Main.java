d916c9d111a9176d3bd77ecd983cc72220e043e4
Serhii <dharm.serg@gmail.com>
2017-02-16 23:15:40 +0200

    how to parse xml

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,12 @@
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("\n Try Recursion:");
-        RecursionTry newRecursionTest = new RecursionTry();
+//        System.out.println("\n Try Recursion:");
+//        RecursionTry newRecursionTest = new RecursionTry();
 
-        System.out.println(newRecursionTest.recursionRun(4));
+//        System.out.println(newRecursionTest.recursionRun(4));
+        System.out.println("\n Try parse xml");
+        HowToWorkWithParser newHowToWorkWithParser = new HowToWorkWithParser();
+        newHowToWorkWithParser.parseXml();
     }
 }
